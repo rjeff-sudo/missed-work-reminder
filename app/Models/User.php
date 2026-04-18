85fcd7e1606b8e1141938a9873fe69158af21f9d
rjeff <fitchcharles989@gmail.com>
2026-04-18 19:10:04 +0300

    add attendance relationship to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function attendance()
+    {
+        return $this->hasMany(\App\Models\Attendance::class);
+    }
 }
