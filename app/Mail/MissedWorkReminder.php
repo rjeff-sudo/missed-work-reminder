6e787614ad6283f7319c226cee665cabe15ec209
rjeff <fitchcharles989@gmail.com>
2026-04-18 13:49:16 +0300

    add missed work reminder mail class

---
+++ app/Mail/MissedWorkReminder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class MissedWorkReminder extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Missed Work Reminder',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.missed-work-reminder',
+        );
+    }
+}
