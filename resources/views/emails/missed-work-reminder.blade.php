d064577da7a96eda2e50770810b6450075915069
rjeff <fitchcharles989@gmail.com>
2026-04-18 13:56:15 +0300

    add missed work reminder email template

---
+++ resources/views/emails/missed-work-reminder.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Missed Work Reminder</title>
+</head>
+<body>
+    <h2>Hello {{ $user->name }},</h2>
+
+    <p>This is a reminder that you were <strong>absent from work today</strong>.</p>
+
+    <p>Please contact your supervisor or HR department to explain your absence.</p>
+
+    <p>If this is a mistake and you did attend work today, please ignore this email.</p>
+
+    <br>
+    <p>Regards,</p>
+    <p><strong>HR Department</strong></p>
+</body>
+</html>
